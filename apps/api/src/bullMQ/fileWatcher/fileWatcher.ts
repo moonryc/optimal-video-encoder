b0d516aac49974d7d1b3dee7e1ffee8b206cbccc
Ryan Moon <ryanmoondevelopment@gmail.com>
2025-12-14 15:11:10 -0600

    set up chokidar

--- apps/api/src/bullMQ/fileWatcher/fileWatcher.ts
+++ apps/api/src/bullMQ/fileWatcher/fileWatcher.ts
@@ -1,7 +1,23 @@
 import { ConversionItem } from '../../db';
+import chokidar from 'chokidar';
+import { CONFIG } from '../../config';
+import { getLoggerByName } from '../../utils/getLoggerByName';
+
+const logger = getLoggerByName('fileWatcher.ts');
 
 export const startFileWatcher = ({queueFile}:{queueFile: (path:ConversionItem["path"])=>Promise<void>}) => {
-
+  chokidar.watch(CONFIG.locationConfig.watchDirectory, {
+    persistent: true,
+    awaitWriteFinish: true,
+  }).on("add", async (filePath:string) => {
+    const validExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.ts', '.webm'];
+    const normalizedPath = filePath.toLowerCase();
+    if (!validExtensions.some(extension => normalizedPath.endsWith(extension))) {
+      logger.info(`👀 Ignoring non-supported file: ${filePath}`);
+      return;
+    }
+    await queueFile(filePath);
+  })
 }
 
 
