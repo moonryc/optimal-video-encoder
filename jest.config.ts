c2e2e663ad670fcb8fbb9f0ecd3fc4bc08d76bf0
Ryan Moon <ryanmoondevelopment@gmail.com>
2025-12-27 17:05:50 -0600

    type fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 const { Config } = require('jest');
 const { getJestProjectsAsync } = require('@nx/jest');
 
-module.exports = async (): Promise<Config> => ({
+module.exports = async (): Promise<typeof Config> => ({
   projects: await getJestProjectsAsync(),
 });
