84d736b7b1a01735c45ffb5b88b26d0c3f134261
Ryan Moon <ryanmoondevelopment@gmail.com>
2025-12-27 19:54:06 -0600

    removes bad package

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 import './styles.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,10 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </QueryClientProvider>
   </StrictMode>
 );
